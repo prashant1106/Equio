863b6dab66bab97d9e179a420efa8557a8910491
Prashant Kumar Jha <jha.prashant1106@gmail.com>
2026-02-15 01:24:07 +0530

    Database tests

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,8 @@
 export const NAV_ITEMS = [
     { href: '/', label: 'Dashboard' },
     { href: '/search', label: 'Search' },
-    // { href: '/watchlist', label: 'Watchlist' },
+    { href: '/watchlist', label: 'Watchlist' },
+    { href: '/news', label: 'News'},
 ];
 
 // Sign-up form select options
