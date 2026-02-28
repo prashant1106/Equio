592e8c3ee66ba83bb73a5d0815eb980949d726e7
Prashant Kumar Jha <jha.prashant1106@gmail.com>
2026-02-28 18:06:37 +0530

    signup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: { children : React.ReactNode }) => {
         <main className="auth-layout">
             <section className="auth-left-section scrollbar-hide-default">
                 <Link href="/" className="auth-logo">
-                    <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className='h-8 w-auto' />
+                    <Image src="/assets/icons/logo.svg" alt="Equiologo" fill className='h-8 w-auto' />
                 </Link>
 
                 <div className="pb-6 lg:pb-8 flex-1">{children}</div>
