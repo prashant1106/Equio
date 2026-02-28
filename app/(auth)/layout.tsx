36215559e60799aee641bf7356314a7a03603fa5
Prashant Kumar Jha <jha.prashant1106@gmail.com>
2026-02-28 18:16:31 +0530

    signup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
 import Image from "next/image";
+import {auth} from "@/lib/better-auth/auth";
+import {headers} from "next/headers";
+import {redirect} from "next/navigation";
 
-const Layout = ({ children }: { children : React.ReactNode }) => {
+const Layout = async ({ children }: { children : React.ReactNode }) => {
+    const session = await auth.api.getSession({ headers: await headers() })
+
+    if(session?.user) redirect('/')
     return (
         <main className="auth-layout">
             <section className="auth-left-section scrollbar-hide-default">
                 <Link href="/" className="auth-logo">
-                    <Image src="/assets/icons/logo.svg" alt="Equiologo" fill className='h-8 w-auto' />
+                    <Image src="/assets/icons/logo.svg" alt="Equiologo" width={120} height={32} className='h-8 w-auto' />
                 </Link>
 
                 <div className="pb-6 lg:pb-8 flex-1">{children}</div>
--- lib/better-auth/auth.ts
+++ lib/better-auth/auth.ts
@@ -1,17 +1,17 @@
 import { betterAuth } from "better-auth";
-import { mongodbAdapter} from "better-auth/adapters/mongodb";
-import { connectToDatabase} from "@/database/mongoose";
-import { nextCookies} from "better-auth/next-js";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { connectToDatabase } from "@/database/mongoose";
+import { nextCookies } from "better-auth/next-js";
 
 let authInstance: ReturnType<typeof betterAuth> | null = null;
 
 export const getAuth = async () => {
-    if(authInstance) return authInstance;
+    if (authInstance) return authInstance;
 
     const mongoose = await connectToDatabase();
     const db = mongoose.connection.db;
 
-    if(!db) throw new Error('MongoDB connection not found');
+    if (!db) throw new Error('MongoDB connection not found');
 
     authInstance = betterAuth({
         database: mongodbAdapter(db as any),
